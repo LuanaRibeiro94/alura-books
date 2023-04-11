f5882336c8c66b717271c5514a36ca73c645b0ba
Luana Ribeiro <luanasribeiro94@gmail.com>
2023-04-11 11:36:37 -0300

    Feature: pesquisando livros

---
+++ src/componentes/Pesquisa/dadosPesquisa.js
@@ -0,0 +1,12 @@
+import hp1 from '../../imagens/hp1.jpeg';
+import amenina from '../../imagens/amenina.jpeg';
+import verity from '../../imagens/verity.jpeg';
+import caraval from '../../imagens/caraval.jpeg'
+
+
+export const livros = [
+  {titulo: 'A menina que roubava livros', id: 1, img:amenina},
+  {titulo: 'Caraval', id: 2, img:caraval},
+  {titulo: 'Harry Potter e a Pedra Filosofal', id: 3, img:hp1},
+  {titulo: 'Verity', id: 4, img:verity}
+];
